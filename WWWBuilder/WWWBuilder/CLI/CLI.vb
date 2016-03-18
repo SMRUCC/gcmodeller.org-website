e3b7b65bdc1feb04a5ccb02c3e5f67a62ac44306
xieguigang <xie.guigang@gmail.com>
2016-03-18 23:10:12 +0800

    add html template builder

---
+++ WWWBuilder/WWWBuilder/CLI/CLI.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Module CLI
+
+    Sub New()
+
+    End Sub
+
+    <ExportAPI("--New.Project", Usage:="--New.Project [--Export <DIR>]")>
+    Public Function NewProject(args As CommandLine.CommandLine) As Integer
+        Dim outDIR As String = args.GetValue("--export", App.CurrentWork)
+
+    End Function
+
+    <ExportAPI("/compile",
+               Usage:="/compile /in <proj.xml> [/out <outDIR>]")>
+    Public Function Compile(args As CommandLine.CommandLine) As Integer
+        Dim inProj As String = args("/in")
+        Dim outDIR As String = args.GetValue("/out", inProj.ParentPath)
+        Dim doc As Project = Project.Load(inProj)
+        Return doc.Build(outDIR).CLICode
+    End Function
+End Module
---
+++ WWWBuilder/WWWBuilder/Project.vb
@@ -0,0 +1,114 @@
+﻿Imports System.Text
+Imports Microsoft.VisualBasic.ComponentModel
+
+Public Class Project : Inherits ITextFile
+
+    Public Property Title As String
+
+    Public Function CreateNew(DIR As String) As Project
+        Dim proj As New Project With {
+            .Title = "New HTML Template Project"
+        }
+        Return proj
+    End Function
+
+    ''' <summary>
+    ''' 项目文件所在的文件夹
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property DIR As String
+        Get
+            Return FilePath.ParentPath
+        End Get
+    End Property
+
+    Public ReadOnly Property PagesDIR As String
+        Get
+            Return DIR & "/pages/"
+        End Get
+    End Property
+
+    Public ReadOnly Property TemplatesDIR As String
+        Get
+            Return DIR & "/templates/"
+        End Get
+    End Property
+
+    Public ReadOnly Property HeaderTemplate As String
+        Get
+            Return TemplatesDIR & "/head.html"
+        End Get
+    End Property
+
+    Public ReadOnly Property FooterTemplate As String
+        Get
+            Return TemplatesDIR & "/footer.html"
+        End Get
+    End Property
+
+    Public ReadOnly Property AssetsDIR As String
+        Get
+            Return DIR & "/assets/"
+        End Get
+    End Property
+
+    Public ReadOnly Property FontsDIR As String
+        Get
+            Return DIR & "/fonts/"
+        End Get
+    End Property
+
+    Public Iterator Function Pages() As IEnumerable(Of String)
+        Dim files = FileIO.FileSystem.GetFiles(PagesDIR, FileIO.SearchOption.SearchTopLevelOnly, "*.html")
+        For Each s As String In files
+            Yield s
+        Next
+    End Function
+
+    ''' <summary>
+    ''' 从项目文件数据之中生成HTML模板
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function Build(outDIR As String) As Boolean
+        Dim head As String = Me.HeaderTemplate.ReadAllText
+        Dim footer As String = Me.FooterTemplate.ReadAllText
+
+        For Each file As String In Pages()
+            Dim name As String = file.BaseName
+            Dim html As String = head & vbCrLf & file.ReadAllText & vbCrLf & footer
+            Dim path As String = outDIR & $"/{name}.html"
+            Call html.SaveTo(path, Encoding:=System.Text.Encoding.UTF8)
+        Next
+
+        Call Process.Start(DIR & "/index.html")
+
+        Return True
+    End Function
+
+    Public Shared Function Load(proj As String) As Project
+        Dim doc As Project = proj.LoadTextDoc(Of Project)(ThrowEx:=False)
+        If doc Is Nothing Then
+            doc = New Project With {
+                .FilePath = proj,
+                .Title = proj.ToFileURL
+            }
+            Call doc.Save()
+        End If
+
+        Call "Place your html content files here.".SaveTo(doc.PagesDIR & "/ReadME.txt")
+        Call "Place your assets content files here.".SaveTo(doc.AssetsDIR & "/ReadME.txt")
+        Call "Place the required web font files here.".SaveTo(doc.FontsDIR & "/ReadME.txt")
+
+        If Not doc.HeaderTemplate.FileExists Then
+            Call "".SaveTo(doc.HeaderTemplate)
+        End If
+        If Not doc.FooterTemplate.FileExists Then
+            Call "".SaveTo(doc.FooterTemplate)
+        End If
+        Return doc
+    End Function
+
+    Public Overrides Function Save(Optional FilePath As String = "", Optional Encoding As Encoding = Nothing) As Boolean
+        Return Me.GetXml.SaveTo(getPath(FilePath), getEncoding(Encoding))
+    End Function
+End Class
