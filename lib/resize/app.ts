804e06743fba64f331a4075defcfcec2356e941e
xieguigang <xie.guigang@gcmodeller.org>
2019-02-26 21:25:22 +0800

    fix object reference bugs

--- lib/resize/app.ts
+++ lib/resize/app.ts
@@ -4,13 +4,11 @@ $ts(function () {
     let size = DOM.clientSize();
     let iframe = document.getElementsByTagName("iframe");
 
-    const px = /\d+/ig;
-
     if (size[0] <= 500) {
         for (var i = 0; i < iframe.length; i++) {
             let frame = iframe.item(i);
-            let w: number = parseInt(px.exec(frame.width)[0]);
-            let h: number = parseInt(px.exec(frame.height)[0]);
+            let w: number = parseInt((/\d+/ig).exec(frame.width)[0]);
+            let h: number = parseInt((/\d+/ig).exec(frame.height)[0]);
 
             iframe.item(i).width = `${size[0]}px`;
             iframe.item(i).height = `${size[0] * h / w}px`;
