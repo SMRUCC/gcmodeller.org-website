e82be82b43b9103de1d7e2df692e708a2a420de0
xieguigang <xie.guigang@gcmodeller.org>
2019-02-26 21:15:12 +0800

    add resize

---
+++ lib/resize/app.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="../linq.d.ts" />
+
+$ts(function () {
+    let size = DOM.clientSize();
+    let iframe = document.getElementsByTagName("iframe");
+
+    const px = /\d+/ig;
+
+    if (size[0] <= 500) {
+        for (var i = 0; i < iframe.length; i++) {
+            let frame = iframe.item(i);
+            let w: number = parseInt(px.exec(frame.width)[0]);
+            let h: number = parseInt(px.exec(frame.height)[0]);
+
+            iframe.item(i).width = `${size[0]}px`;
+            iframe.item(i).height = `${size[0] * h / w}px`;
+        }
+    }
+});
