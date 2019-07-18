87b98bdd466bad4a1279242e4fad442fd91f4867
xieguigang <xie.guigang@gcmodeller.org>
2019-07-18 22:45:54 +0800

    update footers

--- lib/resize/app.ts
+++ lib/resize/app.ts
@@ -1,11 +1,16 @@
 ﻿/// <reference path="../linq.d.ts" />
 
+$ts.mode = Modes.debug;
 $ts(function () {
     let size = DOM.clientSize();
     let iframe = document.getElementsByTagName("iframe");
 
     // fix for padding
     size[0] = size[0] * 0.95;
+
+    if (TypeScript.logging.outputEverything) {
+        console.log(iframe);
+    }
 
     if (size[0] <= 500) {
         for (var i = 0; i < iframe.length; i++) {
@@ -15,15 +20,29 @@ $ts(function () {
 
             frame.width = `${size[0]}px`;
             frame.height = `${size[0] * h / w}px`;
-
-            $ts(function () {
-                // 移除推荐div
-                let div = frame.contentDocument.getElementsByClassName("bilibili-player-video-recommend").item(0)
-                let container = div.parentElement;
-
-                container.removeChild(div);
-
-            }, frame);
         }
     }
+
+    //for (var i = 0; i < iframe.length; i++) {
+    //    let frame = iframe.item(i);
+
+    //    if (TypeScript.logging.outputEverything) {
+    //        console.log(frame);
+    //    }
+
+    //    setTimeout(removeAD(frame), 3000);
+    //}
 });
+
+//function removeAD(frame: HTMLIFrameElement) {
+//    return function () {
+//        $ts(function () {
+//            // 移除推荐div
+//            let div = frame.contentDocument.getElementsByClassName("bilibili-player-video-recommend").item(0)
+//            let container = div.parentElement;
+
+//            container.removeChild(div);
+
+//        }, frame);
+//    }
+//}
