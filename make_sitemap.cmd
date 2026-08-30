d89284c7d539c8db437757c29456632a216ea4f5
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-31 05:35:55 +0800

    Update make_sitemap.cmd

--- make_sitemap.cmd
+++ make_sitemap.cmd
@@ -4,3 +4,5 @@ set scanner="G:\GCModeller\src\runtime\httpd\tools\Sitemap.exe"
 set host="https://gcmodeller.org/"
 
 %scanner% /make --site ./ --host %host% --out ./ --depth 999 --max_urls 3000
+
+pause
